438b7dd60f70156ef9b6b792485fc2cb7708c23d
John Sciacchitano <johnjsciacchitano@gmail.com>
2026-09-12 20:27:02 -0400

    Update TeenyColor to 1.0.1

--- Casks/teenycolor.rb
+++ Casks/teenycolor.rb
@@ -1,8 +1,8 @@
 cask "teenycolor" do
-  version "1.0.0"
-  sha256 "2527b8ea3c2c18a7135048ef431c705008651ea8b211cb4e078249f7888b5085"
+  version "1.0.1,2"
+  sha256 "7d19f9d04066703510a97de442d1f17275a4d68525dca70eed1d991032ea1395"
 
-  url "https://teenycolor.com/downloads/TeenyColor-#{version}.dmg"
+  url "https://teenycolor.com/downloads/TeenyColor-#{version.csv.first}.dmg"
   name "TeenyColor"
   desc "Colour picker and palette manager"
   homepage "https://teenycolor.com/"
@@ -13,12 +13,12 @@ cask "teenycolor" do
   end
 
   auto_updates true
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "TeenyColor.app"
 
   zap trash: [
-    "~/Library/Application Support/TeenyColor",
+    "~/Library/Application Support/com.teenyapps.TeenyColor",
     "~/Library/Caches/com.teenyapps.TeenyColor",
     "~/Library/HTTPStorages/com.teenyapps.TeenyColor",
     "~/Library/Preferences/com.teenyapps.TeenyColor.plist",
